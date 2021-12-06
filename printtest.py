de86ce0b8ecb82411f9fe34a9785e9c7caae85a2
maxthor500 <iraubuntu@gmail.com>
2021-12-06 21:40:13 +0000

    Exercise with the print function

---
+++ printtest.py
@@ -0,0 +1,15 @@
+# Illustration of the print function
+
+# sum of two numbers
+print(3+4)
+
+# show "Hello World!" on two lines
+print("Hello")
+print("World!")
+
+# show diffent values using a single print
+print("My favorite numbers are", 3 + 4, "and", 3 + 10)
+
+print("Goodbye")
+print()
+print("Hope to see you again")
