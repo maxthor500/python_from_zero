349c38ab19afba800fedec150953f334d6d165e9
maxthor500 <iraubuntu@gmail.com>
2022-02-21 19:50:41 +0000

    Create a script sum_10 to sum 10 numbers

---
+++ sum_10.py
@@ -0,0 +1,20 @@
+def sum_of_10(number):
+    """
+    Sum of 10 number starting 
+    to count from the user number
+    """
+    if (number < 0): 
+        print("The input number is less then 0")
+        return 
+    
+    sum_next = number
+
+    for num in range(11):
+        sum_next += num
+
+    print(sum_next)
+
+# test the function
+sum_of_10(10)
+sum_of_10(-5)
+sum_of_10(3)
