eff966ea9f2165ed829f74b6000fb7ef4d817d17
maxthor500 <iraubuntu@gmail.com>
2022-06-09 12:07:21 +0100

    Add solution to the problem n.94 -> Binary Tree Inorder Traversal

---
+++ LeetCode solutions/problem_94.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        
+        inorder_list = []
+        stack = []
+        pointer = root
+        
+        while pointer or stack:
+            while pointer:
+                stack.append(pointer)
+                pointer = pointer.left
+            pointer = stack.pop()
+            inorder_list.append(pointer.val)
+            pointer = pointer.right
+        
+        return inorder_list
+        
+#         def traverse_in_order(root):
+#             if not root:
+#                 return
+            
+#             traverse_in_order(root.left)
+            
+#             inorder_list.append(root.val)
+            
+#             traverse_in_order(root.right)
+        
+#         traverse_in_order(root)
+#         return inorder_list
